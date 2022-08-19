376cfbd1bb32c21c6737b144b29a667277258cec
Adam Steinbach <31391830+steinbacha@users.noreply.github.com>
2022-08-19 15:56:07 -0500

    following Ganesh

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const cors = require('cors');
 const app = express();
 const http = require('http')
 const dns = require('dns');
+const mongoose = require('mongodb');
+const bodyParser = require('body-parser');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -23,6 +25,7 @@ app.get('/api/hello', function(req, res) {
 
 app.use(express.urlencoded());
 
+/*
 app.post('/api/shorturl', (req, res, next) => {
   const URL = require("url").URL;
   const stringIsAValidUrl = (s) => {
@@ -36,7 +39,22 @@ app.post('/api/shorturl', (req, res, next) => {
   stringIsAValidUrl(req.body.url);
   next();
 });
+*/
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
+});
+
+let uri = 'mongodb+srv://asteinbach:' + process.env.PW + '@cluster0.wehkf.mongodb.net/fcc_projects?retryWrites=true&w=majority'
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+let urlSchema = new mongoose.Schema({
+  original_url : {type: String, required: true},
+  short_url: Number
+});
+
+let Url = mongoose.model('Url', urlSchema);
+let responseObject = {};
+app.post('api/shorturl/new', bodyParser.urlencoded({ extended: false }) , (req, res) => {
+  res.json(responseObject)
 });
