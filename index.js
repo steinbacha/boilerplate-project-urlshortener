99f0baa7b5a31efa64976e6bec61eba2dad22f3d
Adam Steinbach <31391830+steinbacha@users.noreply.github.com>
2022-08-19 14:56:39 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const http = require('http')
+const dns = require('dns');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -23,8 +24,11 @@ app.get('/api/hello', function(req, res) {
 app.use(express.urlencoded());
 
 app.post('/api/shorturl', (req, res, next) => {
-  res.json({ test: req.body.url });
-  console.log(req.query.url);
+  if (dns.lookup(req.query.url, err)) {
+    res.json({ original_url: req.body.url, short_url: 'test'});
+  } else {
+    res.json({ error: 'invalid url' })
+  };
   next();
 });
 
