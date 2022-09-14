14ca29d3d569e0ed5f8aa3dec4ae706749eda1b2
Adam Steinbach <31391830+steinbacha@users.noreply.github.com>
2022-09-14 15:21:06 -0500

    starting over..

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+require('mongodb');
 const express = require('express');
 const cors = require('cors');
 const app = express();
@@ -18,42 +19,14 @@ app.get('/', function(req, res) {
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
+app.post('/api/shorturl/new', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
 app.use(express.urlencoded());
 
-/*
-app.post('/api/shorturl', (req, res, next) => {
-  const URL = require("url").URL;
-  const stringIsAValidUrl = (s) => {
-    try {
-      new URL(s);
-      res.json({ original_url: req.body.url, short_url: 'test'}); 
-    } catch (err) {
-      res.json({ error: 'invalid url' })
-    }
-  };
-  stringIsAValidUrl(req.body.url);
-  next();
-});
-*/
-
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
-
-let uri = 'mongodb+srv://asteinbach:' + process.env.PW + '@cluster0.wehkf.mongodb.net/fcc_projects?retryWrites=true&w=majority'
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-
-let urlSchema = new mongoose.Schema({
-  original_url : {type: String, required: true},
-  short_url: Number
-});
-
-let Url = mongoose.model('Url', urlSchema);
-let responseObject = {};
-app.post('api/shorturl/new', bodyParser.urlencoded({ extended: false }) , (req, res) => {
-  res.json(responseObject)
-});
+console.log(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
